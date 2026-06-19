7b7acc992fc27c1d4ca056e708fc3d84a59c4b64
DAntyNoel <dantynoel@sjtu.edu.cn>
2026-06-19 18:42:10 +0800

    删除冗余文件

--- restart_nonebot.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-
-set "PROJECT_ROOT=%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%PROJECT_ROOT%restart_nonebot.ps1" %*
-exit /b %ERRORLEVEL%
--- stopall.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-
-set "PROJECT_ROOT=%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%PROJECT_ROOT%stopall.ps1" %*
-exit /b %ERRORLEVEL%
